08b6244b5158588091493594cce200069afe08ae
YifangZhang <yifangzhang2009@gmail.com>
2016-03-02 02:26:04 -0600

    weather wind updates

--- simple_city_parser.py
+++ simple_city_parser.py
@@ -8,15 +8,17 @@ import xml.etree.ElementTree as ET
 
 
 
-def air_parser(URL, city_name, saved_number):
+def air_parser(URL, URL2, city_name):
 
 	#base_URL = "http://www.stateair.net/web/rss/1/1.xml"
 	#URL = base_URL ##+ "count=" + counting + "&offset=" + offset
 	result = requests.get(URL)
+	result2 = requests.get(URL2)
 
 	#print result.text
 
 	root = ET.fromstring(result.text)
+	#root2 = ET.fromstring(result2.text)
 
 	#city_name = "Beijing"
 
@@ -28,8 +30,23 @@ def air_parser(URL, city_name, saved_number):
 			time = item.find("title").text
 			PM25 = item.find("Conc").text
 			AQI = item.find("AQI").text
-			entry = {"city":city_name ,"time":time, "PM2.5":PM25, "AQI":AQI}
+
+			## from URL2
+			xml_data = result2.text.split('\n')[1:]
+
+			data1 = xml_data[10]
+			data2 = xml_data[12]
+
+			weather = data1.split(': ')[1].split(', ')[0]
+			windSpeed = data2.split('Wind Speed: ')[1].split('mph')[0] #unit mph
+			windDirection = data2.split('Wind Direction: ')[1].split(', ')[0]
+			## end of URL2
+
+			entry = {"city":city_name ,"time":time, "Concentration":PM25, "AQI":AQI, 
+					"weather":weather, "windSpeed":windSpeed, "windDirection":windDirection}
 			temp.append(entry)
+
+			break
 		else:
 			print "this is not an record"
 
@@ -42,18 +59,23 @@ def air_parser(URL, city_name, saved_number):
 			# init the writer #
 			samplewriter = csv.writer(outfile, delimiter=',', quoting=csv.QUOTE_MINIMAL)
 			# saved number should be less than 10 #
-			for i in reversed(range(0, saved_number)):
-				samplewriter.writerow([temp[i]['time'], temp[i]['city'], temp[i]['PM2.5'], temp[i]['AQI']])
+			for i in reversed(range(0, 1)):
+				samplewriter.writerow([temp[i]['time'], temp[i]['city'], temp[i]['Concentration'], 
+									temp[i]['AQI'], temp[i]['weather'], temp[i]['windSpeed'], 
+									temp[i]['windDirection']])
 	else: ## file already exists ##
 		filename_output = city_name + ".csv"
 		with open(filename_output, 'a') as outfile:
 			# init the writer #
 			samplewriter = csv.writer(outfile, delimiter=',', quoting=csv.QUOTE_MINIMAL)
 			# write first line #
-			samplewriter.writerow(['time', 'city', 'PM2.5', 'AQI'])
+			samplewriter.writerow(['Time', 'City', 'Concentration', 'AQI', 
+									'Weather', 'Wind_Direction', 'Wind_Speed'])
 			# saved number should be less than 10 #
-			for i in reversed(range(0, saved_number)):
-				samplewriter.writerow([temp[i]['time'], temp[i]['city'], temp[i]['PM2.5'], temp[i]['AQI']])
+			for i in reversed(range(0, 1)):
+				samplewriter.writerow([temp[i]['time'], temp[i]['city'], temp[i]['Concentration'], 
+									temp[i]['AQI'], temp[i]['weather'], temp[i]['windSpeed'], 
+									temp[i]['windDirection']])
 
 	print city_name + "job is done."
 	#print temp
@@ -63,26 +85,46 @@ def air_parser(URL, city_name, saved_number):
 ##### Main Function #####
 #########################
 
-air_parser('http://www.stateair.net/web/rss/1/1.xml', 'Beijing', 24)
-air_parser('http://www.stateair.net/web/rss/1/2.xml', 'Chengdu', 24)
-air_parser('http://www.stateair.net/web/rss/1/3.xml', 'Guangzhou', 24)
-air_parser('http://www.stateair.net/web/rss/1/4.xml', 'Shanghai', 24)
-air_parser('http://www.stateair.net/web/rss/1/5.xml', 'Shenyang', 24)
+air_parser('http://www.stateair.net/web/rss/1/1.xml', 
+			'http://open.live.bbc.co.uk/weather/feeds/en/1816670/observations.rss', 
+			'Beijing')
+air_parser('http://www.stateair.net/web/rss/1/2.xml', 
+			'http://open.live.bbc.co.uk/weather/feeds/en/1815286/observations.rss',
+			'Chengdu')
+air_parser('http://www.stateair.net/web/rss/1/3.xml',
+			'http://open.live.bbc.co.uk/weather/feeds/en/1809858/observations.rss',
+			'Guangzhou')
+air_parser('http://www.stateair.net/web/rss/1/4.xml',
+			'http://open.live.bbc.co.uk/weather/feeds/en/1796236/observations.rss',
+			'Shanghai')
+air_parser('http://www.stateair.net/web/rss/1/5.xml',
+			'http://open.live.bbc.co.uk/weather/feeds/en/2034937/observations.rss', 
+			'Shenyang')
 
 def job1():
-	air_parser('http://www.stateair.net/web/rss/1/1.xml', 'Beijing', 1)
+	air_parser('http://www.stateair.net/web/rss/1/1.xml', 
+			'http://open.live.bbc.co.uk/weather/feeds/en/1816670/observations.rss', 
+			'Beijing')
 	print "Beijing job is on schedule"
 def job2():
-	air_parser('http://www.stateair.net/web/rss/1/2.xml', 'Chengdu', 1)
+	air_parser('http://www.stateair.net/web/rss/1/2.xml', 
+			'http://open.live.bbc.co.uk/weather/feeds/en/1815286/observations.rss',
+			'Chengdu')
 	print "Chengdu job is on schedule"
 def job3():
-	air_parser('http://www.stateair.net/web/rss/1/3.xml', 'Guangzhou', 1)
+	air_parser('http://www.stateair.net/web/rss/1/3.xml',
+			'http://open.live.bbc.co.uk/weather/feeds/en/1809858/observations.rss',
+			'Guangzhou')
 	print "Guangzhou job is on schedule"
 def job4():
-	air_parser('http://www.stateair.net/web/rss/1/4.xml', 'Shanghai', 1)
+	air_parser('http://www.stateair.net/web/rss/1/4.xml',
+			'http://open.live.bbc.co.uk/weather/feeds/en/1796236/observations.rss',
+			'Shanghai')
 	print "Shanghai job is on schedule"
 def job5():
-	air_parser('http://www.stateair.net/web/rss/1/5.xml', 'Shenyang', 1)
+	air_parser('http://www.stateair.net/web/rss/1/5.xml',
+			'http://open.live.bbc.co.uk/weather/feeds/en/2034937/observations.rss', 
+			'Shenyang')
 	print "Shenyang job is on schedule"
 
 schedule.every(60).minutes.do(job1)
